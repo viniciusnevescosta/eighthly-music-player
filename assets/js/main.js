739acb60599452f17bfb07609f8930e79b7a2870
Vinícius <vinicius.nevesco@gmail.com>
2022-01-17 19:57:50 -0300

    beta 2.0
    This was the best successful attempt when dealing with multiple songs, after 3 days i got a way to change songs without it playing together with others (a complete mess) when clicking on a div.
    
    Before clicking it paused the song, now it plays from the beginning or plays another one if you choose. What is joyful on the one hand, is completely frustrating on the other, this brought me a problem that I didn't have, which is not being able to pause the songs when clicking the play button.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,101 +7,147 @@ const musicas = [
         duracao: '3:30',
         som: 'assets/songs/dojacat.mp3',
         capa: 'url(/assets/imgs/dojacat.avif)',
-    }
+    },
+    {
+        id: 2,
+        nome: 'Måneskin - Beggin',
+        artista: 'Blueberry',
+        duracao: '3:33',
+        som: 'assets/songs/Beggin.mp3',
+        capa: 'url(/assets/imgs/beggin.avif)',
+    },
+    {
+        id: 3,
+        nome: 'Freio da Blazer',
+        artista: 'L7NNON',
+        duracao: '3:00',
+        som: 'assets/songs/Freio.mp3',
+        capa: 'url(/assets/imgs/good4u.avif)',
+    },
+    {
+        id: 4,
+        nome: 'Life Goes On',
+        artista: 'Oliver Tree',
+        duracao: '2:41',
+        som: 'assets/songs/Life.mp3',
+        capa: 'url(/assets/imgs/justice.avif)',
+    },
+    {
+        id: 5,
+        nome: 'Peaches',
+        artista: 'Justin Bieber',
+        duracao: '3:18',
+        som: 'assets/songs/Peaches.mp3',
+        capa: 'url(/assets/imgs/justice.avif)',
+    },
 ];
 
-// Função que cria músicas
-const criarMusica = function(id, nome, artista, duracao, som, img){
-    let pai = document.querySelector('.item1b'); 
-    let div = document.createElement('div');
+// FUNÇÃO: CRIAR MÚSICAS:
+const criarMusica = function(id, nome, artista, duracao, som, capa){
+    // div
+    const div = document.createElement('div');
     div.classList.add('musica');
+
+    // elemento pai
+    const pai = document.querySelector('.item1b'); 
     pai.appendChild(div);
     
+    // subclasses
     for(i = 0; i < 3; i++){
-        let musicItens = document.createElement('div');
-        musicItens.classList.add('musicItens');
-        div.appendChild(musicItens);
+        const sub = document.createElement('div');
+        sub.classList.add('musicItens');
+        div.appendChild(sub);
     };
 
-    let musicName = div.querySelectorAll('.musicItens')[0];
-    let name = document.createElement('p');
-    name.id = 'musicName';
-    name.innerText = `${id}. ${nome}`;
-    musicName.appendChild(name);
+    const pti = div.querySelectorAll('.musicItens')[0];
+    const ptii = div.querySelectorAll('.musicItens')[1];
+    const ptiii = div.querySelectorAll('.musicItens')[2];
 
-    let musicArtist = div.querySelectorAll('.musicItens')[1];
-    let artist = document.createElement('p');
-    artist.id = 'musicArtist';
-    artist.innerText = artista;
-    musicArtist.appendChild(artist);
+    // nome da música
+    const mscName = document.createElement('p');
+    mscName.innerText = `${id}. ${nome}`;
+    pti.appendChild(mscName);
+    
+    // nome do artista
+    const mscArtist = document.createElement('p');
+    mscArtist.innerText = artista;
+    ptii.appendChild(mscArtist);
 
-    let musicDuration = div.querySelectorAll('.musicItens')[2];
-    let duration = document.createElement('p');
-    duration.id = 'musicDuration';
-    duration.innerText = duracao;
-    musicDuration.appendChild(duration);
+    // duracação da música
+    const mscDuration = document.createElement('p');
+    mscDuration.innerText = duracao;
+    ptiii.appendChild(mscDuration);
 
-        let audio = document.createElement('audio');
-        audio.setAttribute('controls', 'controls');
-        
-        let src = document.createElement('source');
-        src.setAttribute('src', som);
-        src.setAttribute('type', 'audio/mpeg');
+    // essenciais
+    const footer = document.querySelector('footer');
+    const play = document.querySelector('#btnPlay');
+    const mscPlay = document.querySelector('#nomeTocando');
+    const artPlay = document.querySelector('#artistaTocando');
+    const albumPlay = document.querySelector('.albumPlay');
+   
+    // criar audio
+    const audio = document.createElement('audio');
+    audio.setAttribute('controls', 'controls');
+    audio.id = id;
+    const src = document.createElement('source');
+    src.setAttribute('type', 'audio/mpeg');
+    audio.appendChild(src);
 
-        audio.appendChild(src);
-        div.appendChild(audio);
-
-    // Events on click
-    let btn = document.querySelector('#btnPlay');
-    let nomeTocando = document.querySelector('#nomeTocando');
-    let artistaTocando = document.querySelector('#artistaTocando');
-    let album = document.querySelector('.albumPlay');
-
-    div.onclick = function(){
-        // tocando
-        if(btn.innerText === 'play_arrow') {
+    // função trocar musica
+    const switchMusic = function(lugar) {
+        const qntMsc = document.querySelectorAll('audio').length;
+        const anterior = document.querySelectorAll('audio')[0];        
+        if(qntMsc === 0) {
+            footer.style.display = 'flex';
+            play.innerText = 'pause'
+            mscPlay.innerText = nome;
+            artPlay.innerText = artista;
+            albumPlay.style.backgroundImage = capa;
+            lugar.appendChild(audio);
+            src.setAttribute('src', som);
+            audio.currentTime = 0;
             audio.play();
-            btn.innerText = 'pause';
-            nomeTocando.innerText = nome;
-            artistaTocando.innerText = artista;
-            album.style.backgroundImage = img;
-        } 
-        // pausado
-        else {
-            audio.pause();
-            btn.innerText = 'play_arrow';
-        };
-
-        btn.onclick = function(){
-            // tocando
-            if(btn.innerText === 'play_arrow') {
-                audio.play();
-                btn.innerText = 'pause';
-                
-            } 
-            // pausado
-            else{
-                audio.pause();
-                btn.innerText = 'play_arrow';
-            };            
-        }
-
-        document.body.onkeyup = function(e) {
-            // tocando
-            if(btn.innerText === 'pause' && e.keyCode == 32) {
-                audio.pause();
-                btn.innerText = 'play_arrow';
-            // pausado
-            } else if(btn.innerText === 'play_arrow' && e.keyCode == 32) {
-                audio.play();
-                btn.innerText = 'pause';
-            };
+        } else if(qntMsc === 1) {
+            lugar.removeChild(anterior);
+            play.innerText = 'pause'
+            mscPlay.innerText = nome;
+            artPlay.innerText = artista;
+            albumPlay.style.backgroundImage = capa;
+            lugar.appendChild(audio);
+            src.setAttribute('src', som);
+            audio.currentTime = 0;
+            audio.play();
+        } else if (qntMsc === 1 && play.innerText === 'pause') {
+            play.innerText = 'play_arrow';
         };
     };
+
+    // evento trocar musica
+    div.addEventListener('click', function() {
+        switchMusic(footer);
+    });
+
+    // evento pausar
+    play.addEventListener('click', function() {
+        let isPlaying = audio.currentTime > 0 && !audio.paused && !audio.ended 
+        && audio.readyState > 2;
+    
+        if (!isPlaying && play.innerText === 'pause') {
+            src.setAttribute('src', som);
+            audio.play();
+        } else {
+            src.setAttribute('src', som);
+            audio.pause();
+        }
+    });
 };
 
-// Argumentar músicas
+// criar músicas
 criarMusica(musicas[0].id, musicas[0].nome, musicas[0].artista, musicas[0].duracao, musicas[0].som, musicas[0].capa);
+criarMusica(musicas[1].id, musicas[1].nome, musicas[1].artista, musicas[1].duracao, musicas[1].som, musicas[1].capa);
+criarMusica(musicas[2].id, musicas[2].nome, musicas[2].artista, musicas[2].duracao, musicas[2].som, musicas[2].capa);
+criarMusica(musicas[3].id, musicas[3].nome, musicas[3].artista, musicas[3].duracao, musicas[3].som, musicas[3].capa);
+criarMusica(musicas[4].id, musicas[4].nome, musicas[4].artista, musicas[4].duracao, musicas[4].som, musicas[4].capa);
 
 // Mostra data atual e quantidade de músicas
 const dataQuantidade = function(){
