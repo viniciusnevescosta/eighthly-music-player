533913974d352d11980f1c6ec75e9171af146ab1
Vinícius <vinicius.nevesco@gmail.com>
2022-01-21 14:10:56 -0300

    Demo 1.0
    This commit contains the biggest changes so far. This fact allows me to launch the first demo version.
    
    HTML: Now the project has an official name! It's called: EighthLy.
    CSS: New color palette that makes the project more original.
    JS: New rewind and song skip functions.
    
    As well as other changes.

--- README.md
+++ README.md
@@ -1,2 +1,160 @@
-# music-app-layout
- 
+# Eighthly
+
+<div id="top"></div>
+
+<!-- PROJECT SHIELDS -->
+[![Contributors][contributors-shield]][https://github.com/Jolonte/EighthLy/graphs/contributors]
+[![Forks][forks-shield]][https://github.com/Jolonte/EighthLy/network/members]
+[![Stargazers][stars-shield]][https://github.com/Jolonte/EighthLy/stargazers]
+[![Issues][issues-shield]][https://github.com/Jolonte/EighthLy/issues]
+[![MIT License][license-shield]][LICENSE]
+[![LinkedIn][linkedin-shield]][https://www.linkedin.com/in/nevesco/]
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/Jolonte/EighthLy">
+    <img src="assets\to_readme\logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">EighthLy (Playlist demo)</h3>
+
+  <p align="center">
+    Free of any kind of royalties, you can listen to music whenever you want.
+    <br />
+    <a href="">Deploy</a>
+    ·
+    <a href="https://github.com/Jolonte/EighthLy/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/Jolonte/EighthLy/pulls">Request Feature</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<img width="800" height="480" src="assets/to_readme/eighthly_git.GIF"
+
+As a good music listener, I know many great music players available, however, few free of any kind of advertisements and streaming subscription to unleash the full power of the player. That's why I decided to create my own player.
+
+Benefits::
+* Always listen with maximum quality.
+* Be free to skip as many songs as you like at any time!
+* Free of advertisements. :smile:
+
+The project is currently in a demo version. Feel free to come back and follow the news.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* [HTML5](https://html5.org/)
+* [CSS3](https://www.w3.org/Style/CSS/Overview.en.html)
+* [JavaScript](https://www.javascript.com/)
+* [Material Icons](https://fonts.google.com/icons)
+* [Animate.css](https://animate.style/)
+* [Kodular](https://www.kodular.io/)
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+- [ ] Auto-play
+- [ ] Change volume
+- [ ] Create an account
+- [ ] Manage playlists - add and remove songs
+- [ ] Mobile app
+- [ ] Multi-language Support
+    - [x] Portuguese
+    - [ ] English
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See <a href="LICENSE">LICENSE</a> for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Vinícius Neves Costa - [@oJolont](https://twitter.com/oJolont) - vinicius.nevesco@gmail.com
+
+Project Link: [https://github.com/Jolonte/EighthLy](https://github.com/Jolonte/EighthLy)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Assignments
+
+* [Favicon](https://www.flaticon.com/br/icone-gratis/nota-musical_727218?term=google%20play%20music&page=1&position=37&page=1&position=37&related_id=727218&origin=tag)
+* [Color palette](https://coolors.co/16bac5-5fbff9-efe9f4-171d1c-5863f8)
+
+* [Playlist photo](https://unsplash.com/photos/50HS3AzJsvQ) by [Neven Krcmarek](https://unsplash.com/@nevenkrcmarek) on Unsplash
+* 1st Song: [It Will Come Back](https://www.youtube.com/watch?v=nJi6FgGKTGc) by [The Stolen Orchestra](https://www.youtube.com/channel/UCa3UQAJkky2ZXVB2JRW9lcw) on Youtube
+* 2nd Song: [Whale](https://www.youtube.com/watch?v=UtbReDCdzQw) by [George & Jonathan](https://www.youtube.com/channel/UC-zfW80uv5a3lrH2JiFvGwA) on Youtube
+* 3rd Song: [Nightingale](https://www.youtube.com/watch?v=UtbReDCdzQw) by [Mr. Songs & Tracks](https://www.youtube.com/channel/UCwgLGxCm7NGP3ECNkKnFgJA/videos) on Youtube
+* 4th Song: [Heart Calligraphy](https://www.youtube.com/watch?v=JhwCP49vu9s) by [Cody Martin](https://www.youtube.com/channel/UCaKBEybQjRBmQbT8qLgMHmQ) on Youtube
+* 5th Song: [Wonderland](https://www.youtube.com/watch?v=TL_2Qc27li4) by [Shimmer](https://www.youtube.com/channel/UCVmYmjbWauqzNf3tqL6QeRA)
+* 6th Song: [Falling Through the Hourglass](https://www.youtube.com/watch?v=i4yRbeiu6M0) by [Sid Acharya](https://www.youtube.com/channel/UCzCJS-SPsHqUHO9oYzh5iTw) on Youtube
+* 7th Song: [Another Time, Another Place](https://www.youtube.com/watch?v=9wdYIokXHaU) by [Sounds Like Sander](https://www.youtube.com/channel/UCOOmq7TUfXuFf8SKPcj07Ag) on Youtube
+* 8th Song: [Summit](https://www.youtube.com/watch?v=7kKyDTa9uTI) by [Gray North](https://www.youtube.com/channel/UCEw876R7zDm1zwcZJb1fezw) on Youtube
+* 9th Song: [Thoughts](https://www.youtube.com/watch?v=QlR-aHHqQmQ) by [Adrián Berenguer](https://www.youtube.com/channel/UCDH--aYQOl8jzSMaW17V94A) on Youtube
+* 10th Song: [Corridors of Time](https://www.youtube.com/watch?v=Pdr5GGzrpCY) by [Malcolm Robinson](https://www.youtube.com/channel/UC5icB83QfmTzxR45f8l0hcg) on Youtube
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,87 @@
 // Músicas
-const musicas = [
+let json = `[
     {
-        id: 1,
-        nome: 'Need to Know',
-        artista: 'Doja Cat',
-        duracao: '3:30',
-        som: 'assets/songs/dojacat.mp3',
-        capa: 'url(/assets/imgs/dojacat.avif)',
+        "id": 1,
+        "nome": "It Will Come Back",
+        "artista": "The Stolen Orchestra",
+        "duracao": "3:38",
+        "som": "assets/songs/msc1.ogg",
+        "capa": "url(/assets/imgs/msc1.avif)"
     },
     {
-        id: 2,
-        nome: 'Måneskin - Beggin',
-        artista: 'Blueberry',
-        duracao: '3:33',
-        som: 'assets/songs/Beggin.mp3',
-        capa: 'url(/assets/imgs/beggin.avif)',
+        "id": 2,
+        "nome": "Whale",
+        "artista": "George & Jonathan",
+        "duracao": "4:15",
+        "som": "assets/songs/msc2.ogg",
+        "capa": "url(/assets/imgs/msc2.avif)"
     },
     {
-        id: 3,
-        nome: 'Freio da Blazer',
-        artista: 'L7NNON',
-        duracao: '3:00',
-        som: 'assets/songs/Freio.mp3',
-        capa: 'url(/assets/imgs/good4u.avif)',
+        "id": 3,
+        "nome": "Nightingale",
+        "artista": "Emorie",
+        "duracao": "4:06",
+        "som": "assets/songs/msc3.ogg",
+        "capa": "url(/assets/imgs/msc3.avif)"
     },
     {
-        id: 4,
-        nome: 'Life Goes On',
-        artista: 'Oliver Tree',
-        duracao: '2:41',
-        som: 'assets/songs/Life.mp3',
-        capa: 'url(/assets/imgs/justice.avif)',
+        "id": 4,
+        "nome": "Heart Calligraphy",
+        "artista": "Cody Martin",
+        "duracao": "3:05",
+        "som": "assets/songs/msc4.ogg",
+        "capa": "url(/assets/imgs/msc4.avif)"
     },
     {
-        id: 5,
-        nome: 'Peaches',
-        artista: 'Justin Bieber',
-        duracao: '3:18',
-        som: 'assets/songs/Peaches.mp3',
-        capa: 'url(/assets/imgs/justice.avif)',
+        "id": 5,
+        "nome": "Wonderland",
+        "artista": "Shimmer",
+        "duracao": "3:46",
+        "som": "assets/songs/msc5.ogg",
+        "capa": "url(/assets/imgs/msc5.avif)"
     },
-];
+    {
+        "id": 6,
+        "nome": "Falling Through the Hourglass",
+        "artista": "Sid Acharya",
+        "duracao": "4:16",
+        "som": "assets/songs/msc6.ogg",
+        "capa": "url(/assets/imgs/msc6.avif)"
+    },
+    {
+        "id": 7,
+        "nome": "Another Time, Another Place",
+        "artista": "Sounds Like Sander",
+        "duracao": "2:09",
+        "som": "assets/songs/msc7.ogg",
+        "capa": "url(/assets/imgs/msc7.avif)"
+    },
+    {
+        "id": 8,
+        "nome": "Summit",
+        "artista": "Gray Northa",
+        "duracao": "1:43",
+        "som": "assets/songs/msc8.ogg",
+        "capa": "url(/assets/imgs/msc8.avif)"
+    },
+    {
+        "id": 9,
+        "nome": "Thoughts",
+        "artista": "Adrián Berenguer",
+        "duracao": "4:34",
+        "som": "assets/songs/msc9.ogg",
+        "capa": "url(/assets/imgs/msc9.avif)"
+    },
+    {
+        "id": 10,
+        "nome": "Corridors of Time",
+        "artista": "Malcolm Robinson",
+        "duracao": "4:08",
+        "som": "assets/songs/msc10.ogg",
+        "capa": "url(/assets/imgs/msc10.avif)"
+    }
+    ]`
+const musicas = JSON.parse(json);
 
 // FUNÇÃO: CRIAR MÚSICAS:
 const criarMusica = function(id, nome, artista, duracao, som, capa){
@@ -89,7 +130,7 @@ const criarMusica = function(id, nome, artista, duracao, som, capa){
     audio.setAttribute('controls', 'controls');
     audio.id = id;
     const src = document.createElement('source');
-    src.setAttribute('type', 'audio/mpeg');
+    src.setAttribute('type', 'audio/ogg');
     audio.appendChild(src);
 
     // função trocar musica
@@ -129,10 +170,24 @@ criarMusica(musicas[1].id, musicas[1].nome, musicas[1].artista, musicas[1].durac
 criarMusica(musicas[2].id, musicas[2].nome, musicas[2].artista, musicas[2].duracao, musicas[2].som, musicas[2].capa);
 criarMusica(musicas[3].id, musicas[3].nome, musicas[3].artista, musicas[3].duracao, musicas[3].som, musicas[3].capa);
 criarMusica(musicas[4].id, musicas[4].nome, musicas[4].artista, musicas[4].duracao, musicas[4].som, musicas[4].capa);
+criarMusica(musicas[5].id, musicas[5].nome, musicas[5].artista, musicas[5].duracao, musicas[5].som, musicas[5].capa);
+criarMusica(musicas[6].id, musicas[6].nome, musicas[6].artista, musicas[6].duracao, musicas[6].som, musicas[6].capa);
+criarMusica(musicas[7].id, musicas[7].nome, musicas[7].artista, musicas[7].duracao, musicas[7].som, musicas[7].capa);
+criarMusica(musicas[8].id, musicas[8].nome, musicas[8].artista, musicas[8].duracao, musicas[8].som, musicas[8].capa);
+criarMusica(musicas[9].id, musicas[9].nome, musicas[9].artista, musicas[9].duracao, musicas[9].som, musicas[9].capa);
+
+// EVENTOS
+// essenciais
+const footer = document.querySelector('footer');
+const play = document.querySelector('#btnPlay');
+const previus = document.querySelector('#btnPrevius');
+const next = document.querySelector('#btnNext');
+
+let msc = document.querySelector('#nomeTocando');
+let art = document.querySelector('#artistaTocando');
+let alb = document.querySelector('.albumPlay');
 
 // evento play/pause
-const play = document.querySelector('#btnPlay');
-
 play.onclick = function() {
     const audio = document.querySelector('audio');
     if(play.innerText === 'pause') {
@@ -141,7 +196,7 @@ play.onclick = function() {
     } else {
         play.innerText = 'pause';
         audio.play();
-    }
+    };
 };
 
 document.body.onkeyup = function(e){
@@ -154,6 +209,66 @@ document.body.onkeyup = function(e){
         audio.play();
     }
 }
+
+// evento previus/skip music
+let num = 0;
+let teste = document.querySelector('h1');
+
+previus.addEventListener('click', () => {
+    const anterior = document.querySelectorAll('audio')[0];
+    let catchAudio = document.querySelector('audio');
+    let id = parseInt(catchAudio.getAttribute('id'));
+
+    num = (id - 1);
+    num--;
+
+    // criar audio
+    const audio = document.createElement('audio');
+    audio.setAttribute('controls', 'controls');
+    const src = document.createElement('source');
+    src.setAttribute('type', 'audio/ogg');
+    audio.appendChild(src);
+
+    if(num >= 0){
+        footer.removeChild(anterior);
+        msc.innerText = musicas[num].nome;
+        art.innerText = musicas[num].artista;
+        alb.style.backgroundImage = musicas[num].capa;
+        footer.appendChild(audio);
+        src.setAttribute('src', musicas[num].som);
+        audio.id = musicas[num].id;
+        audio.currentTime = 0;
+        audio.play();
+    };
+});
+
+next.addEventListener('click', () =>{
+    const anterior = document.querySelectorAll('audio')[0];
+    let catchAudio = document.querySelector('audio');
+    let id = parseInt(catchAudio.getAttribute('id'));
+
+    num = (id - 1);
+    num++;
+
+    // criar audio
+    const audio = document.createElement('audio');
+    audio.setAttribute('controls', 'controls');
+    const src = document.createElement('source');
+    src.setAttribute('type', 'audio/ogg');
+    audio.appendChild(src);
+
+    if(num <= musicas.length){
+        footer.removeChild(anterior);
+        msc.innerText = musicas[num].nome;
+        art.innerText = musicas[num].artista;
+        alb.style.backgroundImage = musicas[num].capa;
+        footer.appendChild(audio);
+        src.setAttribute('src', musicas[num].som);
+        audio.id = musicas[num].id;
+        audio.currentTime = 0;
+        audio.play();
+    };
+});
 
 // Mostra data atual e quantidade de músicas
 const dataQuantidade = function(){
